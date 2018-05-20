c8b3662620248e9331e748e58777192347aec7e6
takumi314 <isospin_symmetry@me.com>
2018-05-20 23:50:04 +0900

    [doc] update comments, print

--- SimpleStore/Persistable.swift
+++ SimpleStore/Persistable.swift
@@ -46,24 +46,29 @@ struct Repository<E> {
 
 extension UserDefaults: Persistable {
 
+    // MARK: - WritableContract
+
     func write<T>(_ array: Array<T>, key: StoreKey) {
         (0..<array.count).forEach { (index: Int) in
             self.set(array[index], forKey: "data_array_\(index)_\(key.rawValue)")
-            print("data_array_\(index): \(T.self)")
+            print("write > data_array_\(index)_\(key.rawValue)")
         }
     }
 
     func write(_ dictionary: [String: Any], key: StoreKey) {
         let data = NSKeyedArchiver.archivedData(withRootObject: dictionary)
         self.set(data, forKey: key.rawValue)
+        print("write > data_dictionary_\(key.rawValue)")
     }
+
+
+    // MARK: - ReadableContract
 
     func read<T>(key: StoreKey) -> Array<T> {
         var index = 0
         var array = [T]()
         while let item = self.object(forKey: "data_array_\(index)_\(key.rawValue)") as? T {
-            print("data_array_\(index): \(T.self)")
-            print(item)
+            print("read < data_array_\(index)_\(key.rawValue)")
             array.append(item)
             index += 1
         }
@@ -72,6 +77,7 @@ extension UserDefaults: Persistable {
 
     func read(key: StoreKey) -> [String: Any] {
         let data = self.data(forKey: key.rawValue)
+        print("read < data_dictionary_\(key.rawValue)")
         let dic = NSKeyedUnarchiver.unarchiveObject(with: data!) as! [String: Any]
         return dic
     }
